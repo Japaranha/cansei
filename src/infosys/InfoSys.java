ef127d2e5c4bc7a49e29eb153a3c9216634357f4
Gustavo Kazuo <kazucalado@gmail.com>
2020-10-21 23:57:30 -0300

    FEDE MUITO

--- src/infosys/InfoSys.java
+++ src/infosys/InfoSys.java
@@ -15,7 +15,7 @@ public class InfoSys {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Fede muito");
     }
     
 }
